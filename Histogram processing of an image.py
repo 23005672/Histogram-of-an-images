ae768132abdc15797b85be3cd43c146b73635dce
RIYA P L <138971519+23005672@users.noreply.github.com>
2024-10-06 14:00:33 +0530

    Update Histogram processing of an image.py

--- Histogram processing of an image.py
+++ Histogram processing of an image.py
@@ -3,13 +3,15 @@
 
 # In[1]:Write your code to find the histogram of gray scale image and color image channels
 
-
+# Developed By: RIYA P L
+# Register Number: 212223240141
+## Input Grayscale Image and Color Image:
 import cv2
 import matplotlib.pyplot as plt
-gray_image = cv2.imread("gray image of flower.jpg")
-color_image = cv2.imread()
+gray_image = cv2.imread("gray_image.jpg")
+color_image = cv2.imread("color_image.jpg",-1)
 cv2.imshow("Gray Image",gray_image)
-
+cv2.imshow("Colour Image",color_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
@@ -17,30 +19,41 @@ cv2.destroyAllWindows()
 # In[2]:Display the histogram of gray scale image and any one channel histogram from color image
 
 
-import matplotlib.pyplot as plt 
-grayscale_image=cv2.imread("gray image of flower.jpg")
-colourscale_image=cv2.imread("color image of flower.jpg")
-hist=cv2.calcHist(grayscale_image,[0],None,[255],[0,255])
-hist1=cv2.calcHist()
+import cv2
+Gray_image = cv2.imread("gray_image.jpg")
+Color_image = cv2.imread("color_image.jpg")
+import matplotlib.pyplot as plt
+gray_hist = cv2.calcHist([Gray_image],[0],None,[256],[0,256])
+color_hist = cv2.calcHist([Color_image],[0],None,[256],[0,256])
 plt.figure()
+plt.imshow(Gray_image)
+plt.show()
 plt.title("Histogram")
-plt.xlabel("")
-plt.ylabel("pixel count")
-plt.stem()
+plt.xlabel("Grayscale Value")
+plt.ylabel("Pixel Count")
+plt.stem(gray_hist)
 plt.show()
 
+plt.imshow(Color_image)
+plt.show()
+plt.title("Histogram of Color Image - Green Channel")
+plt.xlabel("Intensity Value")
+plt.ylabel("Pixel Count")
+plt.stem(color_hist)
+plt.show()
+cv2.waitKey(0)
 
 
 # In[3]:Write the code to perform histogram equalization of the image. 
-
-
-
 import cv2
-import matplotlib.pyplot as plt 
-gi=cv2.imread("gray image of flower.jpg",0)
-colorscale=cv2.imread("color image of flower.jpg")
-g=cv2.resize(gi,(500,400))
-equ=cv2.equalizeHist(gi)
+gray_image = cv2.imread("gray_image",0)
+cv2.imshow('Grey Scale Image',gray_image)
+equ = cv2.equalizeHist(gray_image)
+cv2.imshow("Equalized Image",equ)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
 
 
 
